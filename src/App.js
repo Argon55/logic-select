1ea8bcfcd2c54779bf2171882742794e364571d8
argon <4r6on55wydodo@gmail.com>
2025-03-16 23:17:35 +0700

    [FEATURE] task 2 new feature data sort by ascending

--- src/App.js
+++ src/App.js
@@ -3,34 +3,35 @@ import "./App.css";
 import Select from "react-select";
 
 function App() {
-  // selanjutnya kita tampung hasil resultnya kedalam sebuah state
+  // [1] selanjutnya kita tampung hasil resultnya kedalam sebuah state
   const [allData, setAllData] = useState([]);
 
   const getBerries = async () => {
     const berries = await fetch("https://pokeapi.co/api/v2/berry");
     const value = await berries.json();
-    // karena hasil yang mau kita ambil berada didalam result
-    // karena yang mau diambil untuk dropdown hanya nama nya, maka kita harus mapping dulu resultnya
+    // [1] karena hasil yang mau kita ambil berada didalam result
+    // [1] karena yang mau diambil untuk dropdown hanya nama nya, maka kita harus mapping dulu resultnya
     const result = value.results.map((data) => {
-      // karena kita ingin menyamakan datanya dengan struktur library select jadi kita akan membuat fake json
+      // [1] karena kita ingin menyamakan datanya dengan struktur library select jadi kita akan membuat fake json
       return {
         label: data.name,
         value: data.name,
       };
     });
-    setAllData(result);
+    // [2] urutkan data berdasarkan ascending
+    setAllData(result.sort((a, b) => a.label.localeCompare(b.label)));
   };
 
-  // useEffect digunakan untuk merender data nya
+  // [1] useEffect digunakan untuk merender data nya
   useEffect(() => {
     getBerries();
   }, []);
 
   return (
     <div className="App">
-      {/* import select dan diberi property options dan isi dari options harus berupa array of object */}
-      {/* dan didalam array of object key nya adalah value dan label */}
-      {/* masukkan allData dari proses diatas */}
+      {/* [1] import select dan diberi property options dan isi dari options harus berupa array of object */}
+      {/* [1] dan didalam array of object key nya adalah value dan label */}
+      {/* [1] masukkan allData dari proses diatas */}
       <Select options={allData}></Select>
     </div>
   );
