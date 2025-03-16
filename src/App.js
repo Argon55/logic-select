64461e89c58afea6a3f008ee3e03c50dc7fcc61e
argon <4r6on55wydodo@gmail.com>
2025-03-16 22:20:46 +0700

    [FEATURE] 1. list dropdown with feature input search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Select from "react-select";
 
 function App() {
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* import select dan diberi property options dan isi dari options harus berupa array of object */}
+      {/* dan didalam array of object key nya adalah value dan label */}
+      <Select options={options}></Select>
     </div>
   );
 }
