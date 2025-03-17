6fb479c76148fb8df9d0ab270a05d7d8e0ffcc4f
argon <4r6on55wydodo@gmail.com>
2025-03-17 10:06:48 +0700

    [FEATURE] new feature 3 show output result user Select

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Select from "react-select";
 function App() {
   // [1] selanjutnya kita tampung hasil resultnya kedalam sebuah state
   const [allData, setAllData] = useState([]);
+  // [3] state untuk menyimpan hasil dari user select
+  const [userSelect, setUserSelect] = useState("");
+  // [3] create new trigger untuk menampilkan datanya di html dengan default nya boolean false
+  const [isShow, setIsShow] = useState(false);
 
   const getBerries = async () => {
     const berries = await fetch("https://pokeapi.co/api/v2/berry");
@@ -20,6 +24,9 @@ function App() {
     });
     // [2] urutkan data berdasarkan ascending
     setAllData(result.sort((a, b) => a.label.localeCompare(b.label)));
+
+    // [2] improve urutkan data berdasarkan descending
+    // setAllData(result.sort((a, b) => b.label.localeCompare(a.label)));
   };
 
   // [1] useEffect digunakan untuk merender data nya
@@ -27,12 +34,35 @@ function App() {
     getBerries();
   }, []);
 
+  // [3] value yang akan di trigger oleh button show value
+  const handleSubmit = () => {
+    // [3] beri validasi agar buttonnya bolak balik
+    setIsShow((state) => !state);
+  };
+
+  // [3] trigger untuk mengambil event valuenya
+  const handleChange = (value) => {
+    setUserSelect(value);
+  };
+
   return (
     <div className="App">
+      {/* [3] kita coba tampilkan datanya*/}
+      {/* [3] jika isSHow bernilai true maka akan memunculkan data userSelect */}
+      <h1>{isShow ? userSelect : ""}</h1>
+      {/* [3] button untuk menampilkan hasil dari user select */}
+      {/* [3] tambahkan validasi didalam button */}
+      <button onClick={() => handleSubmit()} disabled={!userSelect}>
+        {isShow ? "Hide Button" : "Show Values"}
+      </button>
       {/* [1] import select dan diberi property options dan isi dari options harus berupa array of object */}
       {/* [1] dan didalam array of object key nya adalah value dan label */}
       {/* [1] masukkan allData dari proses diatas */}
-      <Select options={allData}></Select>
+      {/* [3] tambahkan event onchange */}
+      <Select
+        options={allData}
+        onChange={(e) => handleChange(e.value)}
+      ></Select>
     </div>
   );
 }
